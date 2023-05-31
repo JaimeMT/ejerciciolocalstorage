90462f7b972f10d5d2f126448851ffe524fa3651
Jose David <clavellino1993@gmail.com>
2023-05-31 16:59:59 +0200

    Añadir footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    LoginComponent,
+    RegisterComponent,
+    EmpleadosComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
