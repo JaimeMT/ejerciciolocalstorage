55777b7ed93251f78563fe7710f57b27103ecd70
AYESA\igiraldo <igiraldo@ayesa.com>
2023-06-01 12:49:36 +0200

    LOGIN JS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { EmpleadosComponent } from './components/empleados/empleados.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    EmpleadosComponent,
     NavbarComponent,
-    HomeComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Persona} from '../../interface/person.interface';
+import { UsuarioService } from '../../service/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string= '';
+  error: string= '';
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  login() {
+
+    const usuariosPermitidos: string[] = [];
+
+    var usuarios = this.usuarioService.getUsuarios();
+
+    for (let i = 0; i < usuarios.length; i++) {
+      var usuario =  usuarios[i];
+      if (usuario) {
+        usuariosPermitidos.push(usuario.nombre);
+      }
+    }
+
+    // Verificar si el usuario y la contraseña son válidos
+    if (usuariosPermitidos.includes(this.username) && this.password === 'admin123') {
+    // Inicio de sesión exitoso
+    console.log('Inicio de sesión exitoso');
+    this.error = ''; // Limpiar el mensaje de error
+  } else {
+    // Inicio de sesión fallido
+    console.log('Inicio de sesión fallido');
+    this.error = 'Nombre de usuario o contraseña incorrectos';
+  }
+  }
 }
